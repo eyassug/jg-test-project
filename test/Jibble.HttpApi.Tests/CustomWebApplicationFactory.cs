133f3020567d069210c29db2b3227e27f414269f
Eyassu Getachew <eyassug@gmail.com>
2022-06-14 05:15:06 +0300

    added Integration Test fixture

---
+++ test/Jibble.HttpApi.Tests/CustomWebApplicationFactory.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jibble
+{
+    public class CustomWebApplicationFactory<TStartup>
+    : WebApplicationFactory<TStartup> where TStartup : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                var descriptor = services.SingleOrDefault(
+                    d => d.ServiceType ==
+                        typeof(DbContextOptions<EmployeeDbContext>));
+
+                services.Remove(descriptor);
+
+                services.AddDbContext<EmployeeDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase("InMemoryDb");
+                });
+
+                var sp = services.BuildServiceProvider();
+
+                using (var scope = sp.CreateScope())
+                {
+                    var scopedServices = scope.ServiceProvider;
+                    var db = scopedServices.GetRequiredService<EmployeeDbContext>();
+                    var logger = scopedServices
+                        .GetRequiredService<ILogger<CustomWebApplicationFactory<TStartup>>>();
+
+                    db.Database.EnsureCreated();
+
+                }
+            });
+        }
+    }
+}
---
+++ test/Jibble.HttpApi.Tests/HttpApiIntegrationTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Jibble
+{
+    public class HttpApiIntegrationTests : IClassFixture<CustomWebApplicationFactory<HttpApi.Startup>>
+    {
+
+        CustomWebApplicationFactory<HttpApi.Startup> Factory { get; }
+
+        public HttpApiIntegrationTests(CustomWebApplicationFactory<HttpApi.Startup> factory)
+            => Factory = factory;
+
+        [Theory]
+        [InlineData("/swagger")]
+        [InlineData("/jobs")]
+        public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url)
+        {
+            var client = Factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync(url);
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.Equal("text/html; charset=utf-8",
+                response.Content.Headers.ContentType.ToString());
+        }
+
+        [Theory]
+        [InlineData("/")]
+        [InlineData("/api")]
+        [InlineData("/api/files")]
+        public async Task Get_NonExisting_NonGetEndpointsReturnNotFound(string url)
+        {
+            var client = Factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync(url);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+
+    }
+}
